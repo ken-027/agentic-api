1e376727a47f172f0006e3fd1cc001787601bed3
ken <keanolida7296@gmail.com>
2025-06-30 13:55:48 +0800

    docs(api docs): adjust api docs and added readme

--- src/swagger.ts
+++ src/swagger.ts
@@ -5,7 +5,7 @@ const options = {
     definition: {
         openapi: "3.0.0",
         info: {
-            title: "Portfolio API",
+            title: "Autonomous Agents API",
             version: "1.0.0",
             description: "API Documentation",
         },
